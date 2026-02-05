323e7bae9b1077da28a535fed83c7976ea446762
jyotishman-s <jlp3636joon@gmail.com>
2026-02-05 23:35:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("JS is running");
+window.onload = () => {
+  const greeting = document.getElementById("greeting");
+  const subtext = document.getElementById("subtext");
+  const teddy = document.querySelector(".teddy");
+  const button = document.querySelector(".gift-btn");
+
+  if (!greeting || !subtext || !teddy || !button) {
+    console.error("Missing elements in HTML");
+    return;
+  }
+
+  greeting.innerText = "Hey Chittu 😊";
+  subtext.innerText = "I have something sweet for you 🍫";
+
+  teddy.style.opacity = "1";
+  button.style.opacity = "1";
+};
+
+
